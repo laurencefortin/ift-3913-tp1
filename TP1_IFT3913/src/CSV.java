a153df1c8ca4d4e14e7bd652176f5079a4ada082
Laurence <laurencefortin1996@gmail.com>
2020-10-05 09:45:34 -0400

    ajout de WMC, classe_BC, CC, methode_BC dans le CSV

--- TP1_IFT3913/src/CSV.java
+++ TP1_IFT3913/src/CSV.java
@@ -9,8 +9,8 @@ public class CSV {
 		private static final String NEW_LINE_SEPARATOR = "\n";
 	    
 	    //CSV file header
-	    private static final String FILE_HEADER_CLASSE = "chemin, classe, classe_LOC, classe_CLOC, classe_DC";
-	    private static final String FILE_HEADER_METHODE = "chemin, classe, methode, methode_LOC, methode_CLOC, methode_DC";
+	    private static final String FILE_HEADER_CLASSE = "WMC, classe_BC, chemin, classe, classe_LOC, classe_CLOC, classe_DC";
+	    private static final String FILE_HEADER_METHODE = "CC, methode_BC, chemin, classe, methode, methode_LOC, methode_CLOC, methode_DC";
 	    
 		Map<Integer, Classe> ListeClasse = new HashMap<>();
 		Map<Integer, Methode> ListeMethode = new HashMap<>();
@@ -36,6 +36,10 @@ public class CSV {
 		             
 		            //Write a new student object list to the CSV file
 		            for (Classe classe : ListeClasse.values()) {
+		            	fileWriter.append(String.valueOf(classe.WMC()));  
+		                fileWriter.append(COMMA_DELIMITER);
+		                fileWriter.append(String.valueOf(classe.classe_BC())); 
+		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(classe.getFullFile().getAbsolutePath())); // class Path
 		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(classe)); // class name
@@ -44,7 +48,7 @@ public class CSV {
 		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(classe.classe_CLOC()));
 		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(classe)); // Classe_DC
+		                fileWriter.append(String.valueOf(classe.classe_DC())); // Classe_DC
 		                fileWriter.append(NEW_LINE_SEPARATOR);
 		            }    
 		        } catch (Exception e) {
@@ -81,6 +85,9 @@ public class CSV {
 		             
 		            //Write a new student object list to the CSV file
 		            for (Methode methode : ListeMethode.values()) {
+		            	fileWriter.append(String.valueOf(methode.CC())); 
+		                fileWriter.append(COMMA_DELIMITER);
+		                fileWriter.append(String.valueOf(methode.methode_BC())); 
 		                fileWriter.append(String.valueOf(methode.getFile())); // class Path (SIGNATURE?)
 		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(methode)); // class name
@@ -91,7 +98,7 @@ public class CSV {
 		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(methode.methode_CLOC()));
 		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode)); // Classe_DC
+		                fileWriter.append(String.valueOf(methode.methode_DC())); // Classe_DC
 		                fileWriter.append(NEW_LINE_SEPARATOR);
 		            }    
 		        } catch (Exception e) {

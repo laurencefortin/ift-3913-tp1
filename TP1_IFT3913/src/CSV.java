cac30e20bb15a54102487ede04bd8309b6dc7277
Laurence <laurencefortin1996@gmail.com>
2020-10-06 15:31:16 -0400

    fix du csv methode pour plusieurs classes sans overwrite

--- TP1_IFT3913/src/CSV.java
+++ TP1_IFT3913/src/CSV.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.List;
 
-
 public class CSV {
-	
+	private boolean existe = true;
+
 		//Delimiter used in CSV file
 		private static final String COMMA_DELIMITER = ", ";
 		private static final String NEW_LINE_SEPARATOR = "\n";
@@ -16,10 +16,9 @@ public class CSV {
 	    private static final String FILE_HEADER_CLASSE = "WMC, classe_BC, chemin, classe, classe_LOC, classe_CLOC, classe_DC";
 	    private static final String FILE_HEADER_METHODE = "CC, methode_BC, chemin, classe, methode, methode_LOC, methode_CLOC, methode_DC";
 	    
-		
-		 
 		public CSV(List<Classe> ListeClasse) {
 			 ecrireClasse(ListeClasse);
+			 ecrireMethode(ListeClasse);
 		 }
 		
 		  /**
@@ -56,8 +55,7 @@ public class CSV {
 		                fileWriter.append(COMMA_DELIMITER);
 		                fileWriter.append(String.valueOf(classe.classe_DC())); // Classe_DC
 		                fileWriter.append(NEW_LINE_SEPARATOR);
-		                ecrireMethode(classe);
-		            }    
+		            }   
 		        } catch (Exception e) {
 		            System.out.println("Error in CsvFileWriter !!!");
 		            e.printStackTrace();
@@ -72,17 +70,20 @@ public class CSV {
 		            } 
 		        }
 		    }
-
+ 
 			  /**
 			   * permet d'ecrire le csv de methode 
 			   * @return un fichier csv creer remplis d'informations sur les methodes*/ 
-		    public void ecrireMethode(Classe classe){
+		    public void ecrireMethode(List<Classe> ListeClasse){
 
 				String fileName = "methodes.csv";
 		        FileWriter fileWriter = null;
+		        
 		                 
 		        try {
+		        	
 		            fileWriter = new FileWriter(fileName);
+		            
 		 
 		            //Write the CSV file header
 		            fileWriter.append(FILE_HEADER_METHODE.toString());
@@ -90,26 +91,29 @@ public class CSV {
 		            //Add a new line separator after the header
 		            fileWriter.append(NEW_LINE_SEPARATOR);
 		             
+		            for (Classe classe : ListeClasse) {
+
 		            //Write a new student object list to the CSV file
-		            for (Methode methode : classe.methodes) {
-		  
-		            	fileWriter.append(String.valueOf(methode.CC())); 
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode.methode_BC())); 
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(classe.getFullFile().getAbsoluteFile())); // class Path (SIGNATURE?)
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(classe.getFullFile().getName())); // class name
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode.getSignature()));//method's class
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode.methode_LOC()));
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode.methode_CLOC()));
-		                fileWriter.append(COMMA_DELIMITER);
-		                fileWriter.append(String.valueOf(methode.methode_DC())); // Classe_DC
-		                fileWriter.append(NEW_LINE_SEPARATOR);
-		            }    
+			            for (Methode methode : classe.methodes) {
+			  
+			            	fileWriter.append(String.valueOf(methode.CC())); 
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(methode.methode_BC())); 
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(classe.getFullFile().getAbsoluteFile())); // class Path (SIGNATURE?)
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(classe.getFullFile().getName())); // class name
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(methode.getSignature()));//method's class
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(methode.methode_LOC()));
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(methode.methode_CLOC()));
+			                fileWriter.append(COMMA_DELIMITER);
+			                fileWriter.append(String.valueOf(methode.methode_DC())); // Classe_DC
+			                fileWriter.append(NEW_LINE_SEPARATOR);
+			            }    
+			           }
 		        } catch (Exception e) {
 		            System.out.println("Error in CsvFileWriter !!!");
 		            e.printStackTrace();
